8ac5381136bfa5c151abb79caa5ad503ea00c498
heuulzp <heuulzp@163.com>
2018-08-06 20:47:32 +0800

    双飞翼布局

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <layout></layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Layout from './components/Layout.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Layout
   }
 }
 </script>
 
 <style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+html,body,#app {
+	height: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="layout">
+	  <header>
+		  <h1>可视化编辑页面</h1>
+	  </header>
+	  <div class="wrap">
+		  <div class="middle">
+			  <div class="middle-wrap">
+				  预览区域
+			  </div>
+		  </div>
+		  <div class="left">
+			  标签区域
+		  </div>
+		  <div class="right">
+			  编辑区域
+		  </div>
+	  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.layout {
+	height: 100%;
+}
+header {
+	height: 50px;
+	line-height: 50px;
+	text-align: center;
+}
+.wrap {
+	margin-top: -50px;
+	padding-top: 50px;
+	height: 100%;
+	overflow: hidden;
+}
+.wrap .left {
+	width: 200px;
+	height: 100%;
+	float: left;
+	margin-left: -100%;
+	border: 1px dashed #FF9999;
+}
+.wrap .middle {
+	width: 100%;
+	height: 100%;
+	float: left;
+}
+.middle-wrap {
+	height: 100%;
+	margin: 0 300px 0 200px;
+	border: 1px dashed #009966;
+}
+.wrap .right {
+	width: 300px;
+	height: 100%;
+	float: left;
+	margin-left: -300px;
+	border: 1px dashed #99CCFF;
+}
+</style>
